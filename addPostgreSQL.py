b2670fd48f55dd1f754b7792b2b87afb0d86be9b
Luciano Filho <lvgalvaofilho@gmail.com>
2022-12-08 03:12:16 -0300

    update the insert sql

--- addPostgreSQL.py
+++ addPostgreSQL.py
@@ -1,6 +1,8 @@
 import psycopg2
 import json
 from config import USER, PASSWORD
+from glob import glob
+
 # connect to the database
 conn = psycopg2.connect(
     host="localhost",
@@ -16,27 +18,27 @@ cur = conn.cursor()
 cur.execute("""
     CREATE TABLE IF NOT EXISTS instagram_data (
         brand VARCHAR(255),
-        followers INTEGER,
-        following INTEGER,
-        likes INTEGER,
+        followers VARCHAR(255),
+        following VARCHAR(255),
+        likes VARCHAR(255),
         collection_date DATE
     )
 """)
 
 # insert data into the table
-with open('coleta/2022-12-08_@anitta.json') as f:
-    data = json.load(f)
-    for item in data:
+for file in glob('./coleta/*.json'):
+    with open(file) as f:
+        data = json.load(f)
         cur.execute("""
-            INSERT INTO instagram_data (brand, followers, following, likes, collection_date)
-            VALUES (%s, %s, %s, %s, %s)
-        """, (
-            item['Brand'],
-            item['Followers'],
-            item['Following'],
-            item['Likes'],
-            item['Collection_date']
-        ))
+                INSERT INTO instagram_data (brand, followers, following, likes, collection_date)
+                VALUES (%s, %s, %s, %s, %s)
+            """, (
+                data['Brand'],
+                data['Followers'],
+                data['Following'],
+                data['Likes'],
+                data['Collection_date']
+            ))
 
 # commit the changes
 conn.commit()
