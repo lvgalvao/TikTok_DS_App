14f6fe39da4249c290eab3d28ff4c9c9ebb6ede7
Luciano Filho <lvgalvaofilho@gmail.com>
2022-12-14 14:58:41 -0300

    add googlesheet api

---
+++ GoogleSheet.py
@@ -0,0 +1,22 @@
+import gspread
+from gspread_config import SPREADSHEETS, WORKSHEET
+
+class GoogleSheet:
+    def __init__(self, sheet_name):
+        self.sheet_name = sheet_name
+        self.scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+        self.sa = gspread.service_account(filename="gspread/service_account.json")
+        self.sh = self.sa.open(SPREADSHEETS)
+        self.sheet = self.sh.worksheet(WORKSHEET)
+
+    def get_row(self, row_number):
+        return self.sheet.row_values(row_number)
+
+    def get_all_rows(self):
+        return self.sheet.get_all_values()
+
+    def get_column(self, column_number):
+        return self.sheet.col_values(column_number)
+
+    def get_all_columns(self):
+        return self.sheet.get
