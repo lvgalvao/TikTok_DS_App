bd430ab0773a0f44bd02782c481f7f6bc9bcb819
Luciano Filho <lvgalvaofilho@gmail.com>
2022-12-14 14:59:28 -0300

    add googlesheet module

--- mainTikTok.py
+++ mainTikTok.py
@@ -1,4 +1,5 @@
 from GetDataTikTok import TikTokGet_Meta
+from GoogleSheet import GoogleSheet
 import PutDataIntoS3
 import json
 import boto3
@@ -7,41 +8,14 @@ from datetime import date, datetime
 today = date.today()
 hour = datetime.now().day
 
-contas = [
-        "@flaviacharallo",
-        "@gkay",
-        "@multishow",
-        "@tirullipa",
-        "@keyrochaa",
-        "@tainacostaxis",
-        "@ruivinhademarte",
-        "@badmioficial",
-        "@lorenaqueiroz",
-        "@mcdonalds_br",
-        "@apple",
-        "@cocacolavn",
-        "@cocacola",
-        "@samsungbrasil",
-        "@samsung",
-        "@samsungespana",
-        "@toyota",
-        "@mcdonalds",
-        "@mcdonaldsfrance",
-        "@microsoft",
-        "@bmw",
-        "@disney",
-        "@disneyplus",
-        "@disneyparks",
-        "@disneyplusid",
-        "@disneychannel",
-        "@nike",
-        "@pepsi",
-        "@pepsiindia",
-    ]
-    
+## Get all values from TiktokBrands.spreadsheets
+GS = GoogleSheet(sheet_name="gspread/service_account.json")
+CONTAS = GS.get_all_rows()
+
+## Amazon buckets
 bucket_name = 'tiktokbrands-bucket'
 
-for conta in contas:
+for conta in CONTAS:
     data = TikTokGet_Meta(conta)
     s3 = boto3.resource('s3')
     json_file = json.dumps(data, indent=4) #json.dump(data, outfile, indent=4)
